40423c6e357ee5caabb0d32dc17a29fcb0d6ea79
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2021-05-07 00:14:22 +0200

    Implement the new parameter feature

--- public/discord-bot/original_bot.js
+++ public/discord-bot/original_bot.js
@@ -8,8 +8,8 @@ require('dotenv').config();
 const Discord = require('discord.js');
 
 const bot = new Discord.Client();
-
-const TOKEN= /*a bot Tokenje, nem kozzeteheto az interneten!*/
+/*a bot Tokenje, nem kozzeteheto az interneten!*/
+const TOKEN= 'ODExOTgzNDQxNjg3MDg1MDU3.YC6Igg.pWDOAL1-4ygDfTF7qfFFwzkRaYM'
 
 
 bot.login(TOKEN);
@@ -22,38 +22,35 @@ bot.on('ready', () => {
 //establish socket communication
 const sockett = io();
 
+//handled objects
 var obj;
+var param;
+
+//socket that handles the initial greetings!
 sockett.on('message', message =>{
 
- // outputMessage(message);
   var tbox = document.getElementById("box");
   var cont = tbox.value;
- // console.log(cont);
- // download(JSON.stringify(cont), "json-file-name.json", "text/plain");
- //obj = JSON.stringify(cont);
+
   obj = JSON.stringify(message);
 });
 
-/* 
-function outputMessage()
-{ */
-//  console.log('A bot elkapta az uzenetet: ',message);
-//  console.log(JSON.stringify(message));
-
-
-//}
-sockett.on("Hello", message =>{
-  console.log(message);
+//receives the given parameters in parame
+sockett.on('parame', message =>{
+  param = JSON.stringify(message);
+  console.log(param);
  });
 
+//basic string reverse function
+ function reverse(s){
+  return s.split("").reverse().join("");
+}
 
-
-
-
+// the bot answers to the given parameters with it's inverse
 bot.on('message', msg => {
-  if (msg.content === 'hello') {
-    msg.reply('Há mé, milyenbe ?');
-    msg.channel.send(obj);
+  if (msg.content === param) {
+    msg.reply('A paramétert megkaptam!');
+    msg.channel.send(reverse(param));
     console.log(obj);
     
 
